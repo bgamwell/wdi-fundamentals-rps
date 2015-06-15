0cbd2234623e3b8f07cff3c7bb8b576a1a401553
bgamwell <bgamwell@gmail.com>
2015-06-15 13:18:27 -0700

    Untested rock paper scissors code - still in development.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -7,6 +7,7 @@ function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -25,14 +26,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move = move || getInput();
+    return playerMove = move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move = move || randomplay();
+    return computerMove = move || randomplay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,7 +41,25 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+  if (playerMove == 'rock' && computerMove == 'paper') {
+    winner = 'player';
+  } else if (playerMove == 'rock' && computerMove == 'scissors') {
+    winner = 'player';
+  } else if (playerMove == 'rock' && computerMove == 'rock') {
+    winner = 'tie';
+  } else if (playerMove == 'paper' && computerMove == 'rock') {
+    winner = 'player';
+  } else if (playerMove == 'paper' && computerMove == 'paper') {
+    winner = 'tie';
+  } else if (playerMove == 'paper' && computerMove == 'scissors') {
+    winner = 'computer';
+  } else if (playerMove == 'scissors' && computerMove == 'rock') {
+    winner == 'computer';
+  } else if (playerMove == 'scissors' && computerMove == 'paper') {
+    winner = 'player';
+  } else if (playerMove == 'scissors' && computerMove == 'scissors') {
+    winner = 'tie';
+  }
     return winner;
 }
 
@@ -48,7 +67,35 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    // This function should continue to play Rock Paper Scissors until either the
+    // player or the computer has won five times.
+    // After each 'round', display some text in the console indicating who played
+    // what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+    //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+
+    while (playerWins < 5 && computerWins < 5) {
+
+      getInput();
+      randomPlay();
+      getPlayerMove();
+      getComputerMove();
+      getWinner();
+
+      if (winner == 'player') {
+        playerWins = playerWins++;
+        console.log('Player chose' + playerMove  + ' while Computer chose' + computerMove + '. Player wins this round!');
+        console.log('The score is currently' + playerWins + ' to' + computerWins + '.');
+        }
+
+      else if (winner == 'computer') {
+        computerWins = computerWins++;
+        console.log('Player chose' + playerMove  + ' while Computer chose' + computerMove '. Computer wins this round!');
+        console.log('The score is currently' + playerWins + 'to' + computerWins + '.');
+        }
+      }
+
+    // I'm not entirely sure how this return statement works
     return [playerWins, computerWins];
 }
